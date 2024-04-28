a3d02c114d208a5a8d13e2c7fe151184cad25450
Lycanthropy3301 <kavin.muthuselvan@gmail.com>
2024-04-28 11:45:06 +0530

    Flash update 2 is real

--- dink.py
+++ dink.py
@@ -7,5 +7,5 @@ cur_window = win32gui.GetForegroundWindow()
 
 args = sys.argv[1:]
 subprocess.run(args, shell=True)
-win32gui.FlashWindowEx(cur_window, 0, 2, 500) # It doesn't flash but whatev
+win32gui.FlashWindowEx(cur_window, 15, 5, 400) # Nvm i fixed it
 playsound('notification.wav')
