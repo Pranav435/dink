e7bd024bdbffd7fad7b5eac96cff6af989ccdbea
Lycanthropy3301 <kavin.muthuselvan@gmail.com>
2024-04-28 10:32:03 +0530

    Flash update is real

--- dink.py
+++ dink.py
@@ -1,9 +1,11 @@
 import sys
 import subprocess
 from win32 import win32gui
+import win32con
 from playsound import playsound
+cur_window = win32gui.GetForegroundWindow()
 
 args = sys.argv[1:]
 subprocess.run(args, shell=True)
-# win32gui.FlashWindowEx() no idea what args are supposed to go here (well somewhat of an idea but im too lazy to add rn)
+win32gui.FlashWindowEx(cur_window, 0, 2, 500) # It doesn't flash but whatev
 playsound('notification.wav')
