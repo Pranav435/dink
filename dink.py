50d5e578d3fab8ee7a124b7e1ad4d267932221ef
Lycanthropy3301 <kavin.muthuselvan@gmail.com>
2024-04-30 13:03:31 +0530

    cross platform but we removed the cross

--- dink.py
+++ dink.py
@@ -3,27 +3,46 @@ import subprocess
 import platform
 from playsound import playsound
 
+# FLAGS
+toast = False
+flash = False
+
+def parse(args):
+    global flash, toast
+    if '--toast' in args or '-t' in args:
+        toast = True
+    elif '--flash' in args or '-f' in args:
+        flash = True
+
+allowed_args = {'--toast', '-t', '--flash', '-f'}
+
 args = sys.argv[1:]
-subprocess.run(args, shell=True)
 
-def windows_dink():
-    import win32con
-    from win32 import win32gui
-    cur_window = win32gui.GetForegroundWindow()
-    win32gui.FlashWindowEx(cur_window, 15, 5, 400) # Nvm i fixed it
+for i in range(0,len(args)):
+	if args[i] not in allowed_args:
+		if args[i][0] != "-":
+			command_pointer = i
+			break
+		else:
+			raise Exception(f"{args[i]} is not a valid flag")
 
-def linux_dink():
-    pass
-
-def that_other_os_dink():
-    pass
+parse(args[:command_pointer])
 
 if __name__ == '__main__':
     if platform.system() == 'Windows':
-        windows_dink()
-    elif platform.system() == 'Linux'
-        linux_dink()
+
+        if flash:
+            from win32 import win32gui
+            import win32con
+            cur_window = win32gui.GetForegroundWindow()
+            subprocess.run(args[command_pointer:], shell=True)
+            win32gui.FlashWindowEx(cur_window, 15, 5, 400) # Nvm i fixed it
+        else:
+            subprocess.run(args[command_pointer:], shell=True)
+
+    elif platform.system() == 'Linux':
+        pass
     else:
-        that_other_os_dink()
+        pass
 
     playsound('notification.wav')
