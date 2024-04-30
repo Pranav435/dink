3c312bfdc5587f0711bb3e8748a1b624112e4e6c
Pranav435 <pranavsavla2003@gmail.com>
2024-04-30 15:33:04 +0530

    plz check this update, nuke being launched and flas done

--- dink.py
+++ dink.py
@@ -6,25 +6,33 @@ from playsound import playsound
 # FLAGS
 toast = False
 flash = False
+numflash=5
 
 def parse(args):
-    global flash, toast
-    if '--toast' in args or '-t' in args:
+    global flash, toast, numflash
+    if '-t' in args:
         toast = True
-    elif '--flash' in args or '-f' in args:
+    elif '-f' in args:
         flash = True
 
-allowed_args = {'--toast', '-t', '--flash', '-f'}
+allowed_args = {'-t', '-f'}
 
 args = sys.argv[1:]
 
 for i in range(0,len(args)):
 	if args[i] not in allowed_args:
-		if args[i][0] != "-":
+		if args[i][0] != "-" and not args[i][0] in ["1","2","3","4","5","6","7","8","9","0"]:
 			command_pointer = i
 			break
+		elif args[i][0] in ["1","2","3","4","5","6","7","8","9","0"]:
+			continue
+
 		else:
 			raise Exception(f"{args[i]} is not a valid flag")
+	if args[i]=="-f":
+		flash=True
+		if args[i+1][0] in ["1","2","3","4","5","6","7","8","9","0"]:
+			numflash=int(args[i+1])
 
 parse(args[:command_pointer])
 
@@ -36,13 +44,18 @@ if __name__ == '__main__':
             import win32con
             cur_window = win32gui.GetForegroundWindow()
             subprocess.run(args[command_pointer:], shell=True)
-            win32gui.FlashWindowEx(cur_window, 15, 5, 400) # Nvm i fixed it
+            win32gui.FlashWindowEx(cur_window, 15, numflash, 400) # Nvm i fixed it
         else:
             subprocess.run(args[command_pointer:], shell=True)
+        if toast:
+            from plyer import notification
+            notification.notify(title="Finished",message="Your command has just finished. Go suck something.",timeout=0)
 
     elif platform.system() == 'Linux':
         pass
     else:
         pass
-
-    playsound('notification.wav')
+    try:
+        playsound('notification.wav')
+    except:
+        pass
