59ebe6a5218a316af90862bbe0b89bd86e100cd7
Lycanthropy3301 <kavin.muthuselvan@gmail.com>
2024-04-30 13:06:02 +0530

    The great unmerge (cleaned up and optimized slightly, added flash flag for windows

--- dink.py
+++ dink.py
@@ -3,23 +3,20 @@ import subprocess
 import platform
 from playsound import playsound
 
-toast=False
-command=1
+# FLAGS
+toast = False
+flash = False
+
+def parse(args):
+    global flash, toast
+    if '--toast' in args or '-t' in args:
+        toast = True
+    elif '--flash' in args or '-f' in args:
+        flash = True
+
+allowed_args = {'--toast', '-t', '--flash', '-f'}
+
 args = sys.argv[1:]
-allowed_args={"--toast","-t"}
-for i in range(0,len(args)):
-	if args[i] not in allowed_args:
-		if not args[i][0]=="-":
-			command=i
-			break
-		else:
-			raiseException(f"{args[i]} is not a valid flag")
-
-	if args[i]=="--toast" or args[i]=="-t":
-		toast=True
-
-
-subprocess.run(args[command:], shell=True)
 
 for i in range(0,len(args)):
 	if args[i] not in allowed_args:
@@ -49,6 +46,3 @@ if __name__ == '__main__':
         pass
 
     playsound('notification.wav')
-    if toast:
-        from plyer import notification
-        notification.notify(title="Command done",message="Kavin you suck",timeout=0)
