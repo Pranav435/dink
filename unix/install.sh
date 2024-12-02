afac86d5b9c7e8946136f7111c6731cbcfc14ae0
Pranav435 <pranavsavla2003@gmail.com>
2024-12-02 11:27:20 +0530

    Final release with fixed sh file

--- unix/install.sh
+++ unix/install.sh
@@ -7,19 +7,19 @@ wav_path="../Source/notification.wav"
 # Define the target directory in the PATH (e.g., /usr/local/bin)
 target_dir="/usr/local/bin"
 
-# Move the dink executable to the target directory
+# Copy the dink executable to the target directory
 if [ -f "$dink_path" ]; then
-    sudo mv "$dink_path" "$target_dir/"
+    sudo cp "$dink_path" "$target_dir/"
     sudo chmod +x "$target_dir/dink"  # Make sure it's executable
-    echo "dink executable moved successfully."
+    echo "dink executable copied successfully."
 else
     echo "Error: dink executable not found."
 fi
 
-# Move the notification.wav file to the target directory
+# Copy the notification.wav file to the target directory
 if [ -f "$wav_path" ]; then
-    sudo mv "$wav_path" "$target_dir/"
-    echo "notification.wav moved successfully."
+    sudo cp "$wav_path" "$target_dir/"
+    echo "notification.wav copied successfully."
 else
     echo "Error: notification.wav file not found."
 fi
